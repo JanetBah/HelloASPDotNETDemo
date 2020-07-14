d00d77683fb8b8517eb645b1eeb8d8acf8a1489e
Janet Bah <jadhiambo20@gmail.com>
2020-07-14 13:25:33 -0500

    Added Hello Controller and Views to project

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+
+            return View();
+        }
+    }
+}
